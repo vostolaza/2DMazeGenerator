c0522e0f6df3d2b1146e96f95c4f16829dc27369
vostolaza <victor.ostolaza@utec.edu.pe>
2021-11-30 11:57:24 -0500

    Added border, finish point, validating input

---
+++ README.md
@@ -0,0 +1,3 @@
+## Random Maze Generator
+
+Input an odd number (>3) and the program will return a maze from starting point A (always the top left corner) to point B (the furthest reachable point from A). There can only be one way to get from A to B (no cycles).
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include <time.h>
 
 struct Point {
     enum cellType {
@@ -12,22 +12,27 @@ struct Point {
 
 typedef std::vector<std::vector<Point> > Maze;
 
+// Adds the surrounding walls to the vector
 void addWalls(Point p, std::vector<Point> &walls, Maze maze) {
+    // Left
     if (p.x > 0) {
         if (maze[p.x - 1][p.y].type == Point::Wall) {
             walls.push_back(maze[p.x - 1][p.y]);
         }
     }
+    // Right
     if (p.x < maze.size() - 1) {
         if (maze[p.x + 1][p.y].type == Point::Wall) {
             walls.push_back(maze[p.x + 1][p.y]);
         }
     }
+    // Up
     if (p.y > 0) {
         if (maze[p.x][p.y - 1].type == Point::Wall) {
             walls.push_back(maze[p.x][p.y - 1]);
         }
     }
+    // Down
     if (p.y < maze[0].size() - 1) {
         if (maze[p.x][p.y + 1].type == Point::Wall) {
             walls.push_back(maze[p.x][p.y + 1]);
@@ -35,23 +40,29 @@ void addWalls(Point p, std::vector<Point> &walls, Maze maze) {
     }
 }
 
+// Checks if there is only one possible exit from said point in order to avoid cycles
+// so that there is only one way to reach the exit.
 bool onlyOneExit(Point p, Maze maze) {
     int exits = -1;
+    // Left
     if (p.x > 0) {
         if (maze[p.x - 1][p.y].type != Point::Wall) {
             if (++exits) return false;
         }
     }
+    // Right
     if (p.x < maze.size() - 1) {
         if (maze[p.x + 1][p.y].type != Point::Wall) {
             if (++exits) return false;
         }
     }
+    // Up
     if (p.y > 0) {
         if (maze[p.x][p.y - 1].type != Point::Wall) {
             if (++exits) return false;
         }
     }
+    // Down
     if (p.y < maze[0].size() - 1) {
         if (maze[p.x][p.y + 1].type != Point::Wall) {
             if (++exits) return false;
@@ -61,7 +72,10 @@ bool onlyOneExit(Point p, Maze maze) {
 }
 
 void printMaze(Maze maze){
+    for (int i = 0; i < maze.size()+2; i++) std::cout << "*";
+    std::cout << std::endl;
     for (int i = 0; i < maze.size(); i++) {
+        std:: cout << "*";
         for (int j = 0; j < maze[0].size(); j++) {
             switch(maze[i][j].type){
                 case Point::Wall:
@@ -71,17 +85,20 @@ void printMaze(Maze maze){
                     std::cout << "o";
                     break;
                 case Point::Start:
-                    std::cout << "s";
+                    std::cout << "A";
                     break;
+                case Point::Finish:
+                    std::cout << "B";
             }
         }
-        std::cout << std::endl;
+        std::cout << "*" << std::endl;
     }
-    std::cout << "-------------" << std::endl;
+    for (int i = 0; i < maze.size()+2; i++) std::cout << "*";
 }
 
 void createMaze2D(int n) {
     Maze maze;
+    // Initializes the maze full of walls
     for (int i = 0; i < n; i++) {
         std::vector<Point> row;
         for (int j = 0; j < n; j++) {
@@ -94,27 +111,48 @@ void createMaze2D(int n) {
         maze.push_back(row);
     }
 
+    // Selects the starting point
     maze[0][0].type = Point::Start;
     std::vector<Point> walls;
+    // Adds the surrounding walls to the vector
     addWalls(maze[0][0], walls, maze);
 
+    // Variable that will become the finish point.
+    Point lastPoint;
+
+    // Selects a random wall that is connected to a corridor and if it only has one exit, it becomes a corridor.
+    // The new corridor's walls are added to the walls vector.
+    // The last corridor to be made becomes the finish point.
     while (!walls.empty()) {
         int r = rand() % walls.size();
         Point p = walls[r];
         if (onlyOneExit(p, maze)){
             maze[p.x][p.y].type = Point::Corridor;
             addWalls(p, walls, maze);
+            lastPoint = p;
         }
         walls.erase(walls.begin() + r);
     }
+    maze[lastPoint.x][lastPoint.y].type = Point::Finish;
     printMaze(maze);
 };
 
 
 int main(){
     int n;
-    std::cin >> n;
-    createMaze2D(n);
+    bool validInput = false;
+    while (!validInput) {
+        std::cout << "Enter the size of the maze: ";
+        std::cin >> n;
+        if (n > 3 && n%2 != 0) {
+            validInput = true;
+        }
+        else {
+            std::cout << "Invalid input. Please enter an odd number greater than 3." << std::endl;
+        }
+    }
+    srand (time(NULL));
+    createMaze2D(n-2);
 
     return 0;
 }
